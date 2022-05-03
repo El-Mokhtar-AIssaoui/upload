83c1415cdd33c8c9c0de9dcf5f12510b50f2298f
El Mokhtar AISSAOUI <marcbiggs1098@gmail.com>
2022-05-03 14:59:33 +0200

    t

--- upload.sh
+++ upload.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 cd /upload
 sudo systemctl stop ElaGatewayApiRest.service
-sudo cp -r /upload/appweb/* /var/www/html/
-sudo cp- r /upload/apiweb/* /home/pi/PocWebApi
+sudo cp -r /upload-main/appweb/* /var/www/html/
+sudo cp -r /upload-main/apiweb/* /home/pi/PocWebApi/
 sudo systemctl start ElaGatewayApiRest.service
 sudo reboot
